863a27fdc88331e2321b940ccebb3bb573b2b737
Shaunak04 <shaunak.deshpande4@gmail.com>
2022-05-31 03:32:28 +0530

    download file code completed

--- scripts/main.py
+++ scripts/main.py
@@ -3,18 +3,31 @@ import urllib.request
 import os
 import subprocess
 from pynpm import NPMPackage
+import requests
 
 class Wrapper:
 
+    # data members
+    df = None
+    path = None
+
+    # constructor
+    def __init__(self):
+        global df
+        global path
+        df = None
+        path = '../inputs/'
+        
+    # CLI loop
     def loop(self):
         print("-----Welcome to de-py-ndency bot:-----\n")
         print("You can start by entering 'help'")
-        while(1):
+        while(True):
             command = input("Enter your command:")
             if(command=="help"):
                 print("""List of valid commands:
-                1. depy -i filname.csv package@version
-                2. depy -i update filename.csv package@version
+                1. depy -i filename.csv package@version
+                2. depy -i -update filename.csv package@version
                 3. exit
                 4. documentation""")
                 pass
@@ -22,12 +35,15 @@ class Wrapper:
             elif(command=="exit"):
                 print("Thanks for using de-py-ndency bot")
                 os._exit(0)
+
             else:
                 lst = command.split(" ")
-                print(lst)
                 if("-update" in lst):
+                    file_name = lst[2]
+                    df = pd.read_csv(path + file_name)
                     # CLONE REPO, USE NPM UPDATE, CREATE A PR
                     continue
+
                 else:
                     if((len(lst)<4) or (lst[0]!='depy') or (lst[1]!='-i')):    
                         print("invalid command")
@@ -35,58 +51,44 @@ class Wrapper:
                     
                     else:
                         file_name = lst[2]
+                        df = pd.read_csv(path + file_name)
                         if(file_name[len(file_name)-3:]!="csv"):
                             print("invalid command")
                             continue
                         
+                        rows, cols = df.shape
                         packages = lst[3:]
                         for pkg in packages:
+                            raw_path = "https://raw.githubusercontent.com/"
                             at_index = pkg.find('@')
                             dependency = pkg[0:at_index]
                             curr_version = pkg[at_index+1:]
                             print(dependency, curr_version)
-                            # npm_call = 'npm show ' + dependency + ' version' 
-                            # latest_ver = subprocess.check_output(npm_call, shell=True)
-                            # output = latest_ver.decode('utf-8')
-                            print()
+                            npm_call = 'npm show ' + dependency + ' version' 
+                            latest_ver = subprocess.check_output(npm_call, shell=True)
+                            output = latest_ver.decode('utf-8')
+                            
+                            for i in range(rows):                            
+                                repo_name = df.iloc[i, 0]
+                                repo_url = raw_path + df.iloc[i, 1][19:] +'/main/package.json'
+                                print(repo_url)
+
+                                # try:
+                                fetch_url = urllib.request.urlopen(repo_url)
+                                con_status = fetch_url.getcode()
+                                r = requests.get(repo_url, allow_redirects=True)
+                                with open('package.json', "wb") as file:
+                                    # get request
+                                    response = requests.get(repo_url)
+                                    # write to file
+                                    file.write(response.content)
+                                print("Connection succesful for project: " + repo_name)
+
+                                # except:
+                                    # print("Invalid URL for project: "+ repo_name)
+                        
                     
 
-
-class Parser:
-    old_version_count = 0
-
-class URL_connection:
-    
-    def connect(self, df):
-        rows, cols = df.shape
-        for i in range(rows):
-            # for j in range(cols):
-            
-            repo_name = df.iloc[i, 0]
-            repo_url = df.iloc[i, 1]
-            
-            try:
-                fetch_url = urllib.request.urlopen(repo_url)
-                con_status = fetch_url.getcode()
-                print("Connection succesful for project: " + repo_name)
-
-            except:
-                print("Invalid URL for project: "+ repo_name)
-
-
-
-class Input_parsing:
-
-    default = False
-
-    def main(*argv):
-        path = '../inputs/inputt.csv'
-        df = pd.read_csv(path)
-        return df
-        
-
 if __name__ == "__main__":
-    inp = Input_parsing()
-    wp = Wrapper()
-    wp.loop()
-    # inp.main()
+    inp = Wrapper()
+    inp.loop()
