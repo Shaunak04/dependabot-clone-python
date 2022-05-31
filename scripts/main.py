d1f006b50239cb3e9a58333bd3f2ff5d2b0e05dd
Shaunak04 <shaunak.deshpande4@gmail.com>
2022-05-31 11:12:45 +0530

    tracking complete

--- scripts/main.py
+++ scripts/main.py
@@ -4,13 +4,15 @@ import os
 import subprocess
 from pynpm import NPMPackage
 import requests
+import json
+
 
 class Wrapper:
 
     # data members
     df = None
     path = None
-
+    logged_in = False
     # constructor
     def __init__(self):
         global df
@@ -23,7 +25,7 @@ class Wrapper:
         print("-----Welcome to de-py-ndency bot:-----\n")
         print("You can start by entering 'help'")
         while(True):
-            command = input("Enter your command:")
+            command = input("Enter your command: ")
             if(command=="help"):
                 print("""List of valid commands:
                 1. depy -i filename.csv package@version
@@ -33,7 +35,7 @@ class Wrapper:
                 pass
 
             elif(command=="exit"):
-                print("Thanks for using de-py-ndency bot")
+                print("Thanks for using de-py-ndency bot\n")
                 os._exit(0)
 
             else:
@@ -46,14 +48,16 @@ class Wrapper:
 
                 else:
                     if((len(lst)<4) or (lst[0]!='depy') or (lst[1]!='-i')):    
-                        print("invalid command")
+                        print("invalid command\n")
                         continue
                     
                     else:
+                        status = []
+                        satisfy = []
                         file_name = lst[2]
                         df = pd.read_csv(path + file_name)
                         if(file_name[len(file_name)-3:]!="csv"):
-                            print("invalid command")
+                            print("invalid command\n")
                             continue
                         
                         rows, cols = df.shape
@@ -71,23 +75,47 @@ class Wrapper:
                             for i in range(rows):                            
                                 repo_name = df.iloc[i, 0]
                                 repo_url = raw_path + df.iloc[i, 1][19:] +'/main/package.json'
-                                print(repo_url)
 
-                                # try:
-                                fetch_url = urllib.request.urlopen(repo_url)
-                                con_status = fetch_url.getcode()
-                                r = requests.get(repo_url, allow_redirects=True)
-                                with open('package.json', "wb") as file:
-                                    # get request
-                                    response = requests.get(repo_url)
-                                    # write to file
-                                    file.write(response.content)
-                                print("Connection succesful for project: " + repo_name)
+                                try:
+                                    fetch_url = urllib.request.urlopen(repo_url)
+                                    con_status = fetch_url.getcode()
+                                    r = requests.get(repo_url, allow_redirects=True)
+                                    with open('package.json', "wb") as file:
+                                        # get request
+                                        response = requests.get(repo_url)
+                                        # write to file
+                                        file.write(response.content)
+                                        status.append("successfull connection")
 
-                                # except:
-                                    # print("Invalid URL for project: "+ repo_name)
-                        
-                    
+                                    try:
+                                        with open('package.json') as json_file:
+                                            data = json.load(json_file)
+                                            current_version = data['dependencies'][dependency]
+                                            if(len(current_version)==0):
+                                                raise Exception("invalid version")
+                                            
+                                            if(current_version[0]=='^' or current_version[0]=='~'):
+                                                current_version = current_version[1:]
+
+                                            satisfy.append(current_version >= curr_version)
+
+                                    except:
+                                        print("Couldn't open package.json")
+                                        satisfy.append(False)
+
+                                        
+                                except:
+                                    status.append("invalid url")
+                                    satisfy.append(False)
+                        print(status)
+                        print(satisfy)
+                        df['tracking_status'] = status
+                        df['satisfy'] = satisfy
+                        df.to_csv("../outputs/output.csv", index = False) 
+
+
+                                
+                            
 
 if __name__ == "__main__":
     inp = Wrapper()
